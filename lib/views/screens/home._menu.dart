88c4714f34949d75db9dc1b22fd1513fcca8b4ce
HWSEO-AKINOKAMI-53183 <hwseo-akinokami-53183@admindeMac-mini.local>
2024-07-18 13:02:55 +0800

    phk

---
+++ lib/views/screens/home._menu.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:football_score/controller/home_controller.dart';
+import 'package:football_score/views/screens/matches/matches_scren.dart';
+import 'package:football_score/views/screens/news/news_screen.dart';
+import 'package:football_score/views/screens/results/result_screen.dart';
+import 'package:football_score/views/screens/upcoming/upcoming_screen.dart';
+
+class Home extends StatelessWidget {
+  Home({super.key});
+
+  final TextStyle unselectedLabelStyle = TextStyle(
+      color: Colors.white.withOpacity(0.5),
+      fontWeight: FontWeight.w500,
+      fontSize: 12);
+
+  final TextStyle selectedLabelStyle = const TextStyle(
+      color: Colors.white, fontWeight: FontWeight.w500, fontSize: 12);
+
+  buildBottomNavigationMenu(context, landingPageController) {
+    return Obx(() => MediaQuery(
+        data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+        child: SizedBox(
+          height: 60,
+          child: BottomNavigationBar(
+            showUnselectedLabels: true,
+            showSelectedLabels: true,
+            onTap: landingPageController.changeTabIndex,
+            currentIndex: landingPageController.tabIndex.value,
+            backgroundColor: const Color.fromRGBO(36, 54, 101, 1.0),
+            unselectedItemColor: Colors.white.withOpacity(0.5),
+            selectedItemColor: Colors.white,
+            unselectedLabelStyle: unselectedLabelStyle,
+            selectedLabelStyle: selectedLabelStyle,
+            items: [
+              BottomNavigationBarItem(
+                icon: Container(
+                  margin: const EdgeInsets.only(bottom: 7),
+                  child: const Icon(
+                    Icons.home,
+                    size: 20.0,
+                  ),
+                ),
+                label: 'Matches',
+                backgroundColor: const Color.fromRGBO(36, 54, 101, 1.0),
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  margin: const EdgeInsets.only(bottom: 7),
+                  child: const Icon(
+                    Icons.calendar_month,
+                    size: 20.0,
+                  ),
+                ),
+                label: 'Upcoming',
+                backgroundColor: const Color.fromRGBO(36, 54, 101, 1.0),
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  margin: const EdgeInsets.only(bottom: 7),
+                  child: const Icon(
+                    Icons.location_history,
+                    size: 20.0,
+                  ),
+                ),
+                label: 'Results',
+                backgroundColor: const Color.fromRGBO(36, 54, 101, 1.0),
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  margin: const EdgeInsets.only(bottom: 7),
+                  child: const Icon(
+                    Icons.settings,
+                    size: 20.0,
+                  ),
+                ),
+                label: 'News',
+                backgroundColor: const Color.fromRGBO(36, 54, 101, 1.0),
+              ),
+            ],
+          ),
+        )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final HomeController homeController =
+        Get.put(HomeController(), permanent: false);
+    return SafeArea(
+        child: Scaffold(
+      bottomNavigationBar: buildBottomNavigationMenu(context, homeController),
+      body: Obx(() => IndexedStack(
+            index: homeController.tabIndex.value,
+            children: const [
+              MatchesScren(),
+              UpcomingScreen(),
+              ResultScreen(),
+              NewsScreen(),
+            ],
+          )),
+    ));
+  }
+}
